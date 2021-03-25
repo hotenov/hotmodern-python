57a662d7ccee6ee295b938a52f52964efdd50e10
Artem Hotenov <qa@hotenov.com>
2021-03-25 17:00:04 +0300

    feat(cli): :sparkles: Add option --language to select Wikipedia edition
    with test if console.main uses the specified language
    Add fixture with wikipedia.random_page mock

--- src/hotmodern_python/console.py
+++ src/hotmodern_python/console.py
@@ -7,11 +7,19 @@ from . import __version__, wikipedia
 
 
 @click.command()
+@click.option(
+    "--language",
+    "-l",
+    default="en",
+    help="Language edition of Wikipedia",
+    metavar="LANG",
+    show_default=True,
+)
 @click.version_option(version=__version__)
-def main():
+def main(language):
     """The hotmodern Python project."""
     try:
-        data = wikipedia.random_page()
+        data = wikipedia.random_page(language=language)
     except requests.exceptions.HTTPError:
         print("API endpoint is unavailable, check API URL or try later")
     else:
