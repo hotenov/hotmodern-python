95b5353ad9299ae5fdd6fa533bcfdb8bc88c2586
Artem Hotenov <qa@hotenov.com>
2021-03-25 15:44:12 +0300

    refactor(cli): :recycle: Move Wikipedia client to separate module

--- src/hotmodern_python/console.py
+++ src/hotmodern_python/console.py
@@ -3,23 +3,22 @@ import textwrap
 import click
 import requests
 
-from . import __version__
+
+from . import __version__, wikipedia
 
 
-API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
 
 @click.command()
 @click.version_option(version=__version__)
 def main():
     """The hotmodern Python project."""
     try:
-        with requests.get(API_URL) as response:
-            response.raise_for_status()
-            data = response.json()
+        data = wikipedia.random_page()
     except requests.exceptions.HTTPError:
         print("API endpoint is unavailable, check API URL or try later")
     else:
         title = data["title"]
         extract = data["extract"]
+        
         click.secho(title, fg="green")
         click.echo(textwrap.fill(extract))
