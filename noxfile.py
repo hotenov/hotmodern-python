ac8a57288b5b46a47eaccd3f2e51c325e1122fdf
Artem Hotenov <qa@hotenov.com>
2021-03-26 08:58:36 +0300

    chore(linting): :wrench: Add flake8 linting into nox session

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,17 @@
 import nox
 
+
+locations = "src", "tests", "noxfile.py"
+
 @nox.session(python=["3.9.2"], reuse_venv=True)
 def tests(session):
     args = session.posargs or ["--cov", "-m", "not e2e"]
     session.run("poetry", "install", external=True)
     session.run("pytest", *args)
+
+
+@nox.session(python=["3.9.2"], reuse_venv=True)
+def lint(session):
+    args = session.posargs or locations
+    session.install("flake8")
+    session.run("flake8", *args)
