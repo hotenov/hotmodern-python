cd6b270e1c7f7eb18fc09e9bd7a5c4b5bde2ae45
Artem Hotenov <qa@hotenov.com>
2021-03-28 08:53:06 +0300

    refactor(typing): :recycle: Add mypy default Nox session

--- noxfile.py
+++ noxfile.py
@@ -4,7 +4,7 @@ import tempfile
 import nox
 
 
-nox.options.sessions = "lint", "safety", "tests"
+nox.options.sessions = "lint", "mypy", "safety", "tests"
 locations = "src", "tests", "noxfile.py"
 
 
@@ -75,3 +75,10 @@ def safety(session):
         install_with_constraints(session, "safety")
         session.run("safety", "check", f"--file={requirements.name}", "--full-report")
     pathlib.Path(requirements.name).unlink()
+
+
+@nox.session(python=["3.9.2"])
+def mypy(session):
+    args = session.posargs or locations
+    install_with_constraints(session, "mypy")
+    session.run("mypy", *args)
