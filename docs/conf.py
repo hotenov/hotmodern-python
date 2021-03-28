b008520e065f51a6247e014bc35dddf8f87038a1
Artem Hotenov <qa@hotenov.com>
2021-03-28 20:39:45 +0300

    docs: :memo: Add Reference page with auto generated API documentation

--- docs/conf.py
+++ docs/conf.py
@@ -2,3 +2,8 @@
 project = "hotrmodern-python"
 author = "Artem Hotenov"
 copyright = f"2021, {author}"
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+]
