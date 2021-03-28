604a0b32b5c4593240f0ac2b8896177258dfdfe6
Artem Hotenov <qa@hotenov.com>
2021-03-28 20:10:01 +0300

    docs: :hammer: Add docs folder and Sphinx build session

---
+++ docs/conf.py
@@ -0,0 +1,4 @@
+"""Sphinx configuration."""
+project = "hotrmodern-python"
+author = "Artem Hotenov"
+copyright = f"2021, {author}"
---
+++ docs/index.rst
@@ -0,0 +1,2 @@
+This is docs/index.rst,
+documenting the hotmodern Python project.
